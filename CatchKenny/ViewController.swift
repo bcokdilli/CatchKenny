05930e319af9dfd80b98b855459e753e89f61cfb
Bahadir Cokdilli <b.cokdilli@hotmail.com.tr>
2022-10-22 14:57:11 +0300

    Initial Commit

---
+++ CatchKenny/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatchKenny
+//
+//  Created by Bahadır Çokdilli on 22.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
